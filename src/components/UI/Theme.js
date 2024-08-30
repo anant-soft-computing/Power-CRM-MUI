32ac3d65a241a32f7d23fd1388cd4b452718a89c
ASCDarshan <ascdarshan@gmail.com>
2024-08-30 17:15:32 +0530

    make field size small - change color theme -add general document

--- src/components/UI/Theme.js
+++ src/components/UI/Theme.js
@@ -1,15 +1,37 @@
-import { createTheme } from '@mui/material/styles';
+import { createTheme } from "@mui/material/styles";
 
 const Theme = createTheme({
-    palette: {
-        primary: {
-            main: '#1976d2',
-            dark: '#115293',
-        },
-        secondary: {
-            main: '#dc004e',
-        },
+  palette: {
+    type: "dark",
+    primary: {
+      main: "#1976d2",
     },
+    secondary: {
+      main: "#f50057",
+    },
+    info: {
+      main: "#000000",
+    },
+    warning: {
+      main: "#7f5217",
+    },
+  },
+  direction: "rtl",
+  shape: {
+    borderRadius: 4,
+  },
+  spacing: 8,
+  props: {
+    MuiList: {
+      dense: true,
+    },
+    MuiMenuItem: {
+      dense: true,
+    },
+    MuiTable: {
+      size: "small",
+    },
+  },
 });
 
 export default Theme;
